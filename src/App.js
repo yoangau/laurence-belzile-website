b4be5d071411a0312d6123be78011c57a8e6389a
yoan gauthier <yoan.gauthier@polymtl.ca>
2019-09-02 19:58:34 -0400

    scroll to top on reload

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import NavBar from './components/NavBar';
 import Work from './components/Work'
 import Contact from './components/Contact'
+import { animateScroll as scroll } from "react-scroll";
 
 
+export default class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar />
-      <Work />
-      <Contact />
-    </div>
-  );
+  componentDidMount() {
+    scroll.scrollToTop();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+        <Work />
+        <Contact />
+      </div>
+    )
+  }
 }
-
-export default App;
