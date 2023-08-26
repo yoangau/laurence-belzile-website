b88bff40265b2d2bc2260b4e6e57d3e5fc1f6f97
Yoan Gauthier <yoangau@gmail.com>
2023-08-26 14:35:51 -0700

    Version pdf du cv au debut de la page cv

--- src/constants/folders.js
+++ src/constants/folders.js
@@ -1,3 +1,4 @@
 export const PROJECTS_FOLDER = '/projects';
 export const COMPRESSED_FOLDER = PROJECTS_FOLDER + '/compressed';
 export const PLACEHOLDER_FOLDER = PROJECTS_FOLDER + '/placeholders';
+export const RESUME_FOLDER = '/resume';
